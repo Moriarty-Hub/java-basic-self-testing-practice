fed4c0f3b81305bef0c926ae9605756be884db6a
Xiangrui Dong <samdongxiangrui@outlook.com>
2020-07-28 09:53:21 +0800

    [Xiangrui Dong] feat: finish MyStack.java

--- src/main/java/com/twc/javaBasic/MyStack.java
+++ src/main/java/com/twc/javaBasic/MyStack.java
@@ -24,7 +24,8 @@ public class MyStack {
 
         // TODO: Please push the value into the storage here.
         // <--start
-
+        storage[count] = value;
+        count++;
         // --end-->
     }
 
@@ -37,7 +38,10 @@ public class MyStack {
         //  IMPORTANT: You SHOULD NOT USE COLLECTIONS (such as List<T>) OTHER THAN ARRAY.
         //
         // <--start
-
+        int[] newArray = new int[newCapacity];
+        System.arraycopy(storage, 0, newArray, 0, capacity);
+        capacity = newCapacity;
+        storage = newArray;
         // --end-->
     }
 
@@ -57,9 +61,8 @@ public class MyStack {
         //  Please pop one element from the array. You should update the `count` field
         //  as well.
         // <--start
-
+        count--;
+        return storage[count];
         // --end-->
-
-        throw new UnsupportedOperationException("Stack is empty.");
     }
 }
